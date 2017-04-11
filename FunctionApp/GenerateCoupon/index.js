3365388a9025ed11bcf72cb7a1a5c06e171b51c0
Ahmed Sabbour <sabb0ur@gmail.com>
2017-04-11 22:17:48 +0200

    updated function

--- FunctionApp/GenerateCoupon/index.js
+++ FunctionApp/GenerateCoupon/index.js
@@ -1,21 +1,23 @@
-﻿var Jimp = require("jimp");
-var path = require("path");
-var azure = require('azure-storage');
+﻿//var Jimp = require("jimp");
+//var path = require("path");
+//var azure = require('azure-storage');
 
-module.exports = function (context, data) {
+module.exports = function (context, req) {
 
     context.log('Function running');
 
     try {
-        var customerName = data.body.CustomerName;
+        var customerName = req.body.CustomerName;
 
         // Log incoming request:
         context.log('Got a request to create coupon for customer: ' + customerName);
-        /*
+
+
         // Determine where base image is:
         var baseImgPath = path.resolve(__dirname, 'coupon.jpg');
         context.log('Base image path: ' + baseImgPath);
 
+        /*
         // Read image with Jimp
         Jimp.read(baseImgPath).then((image) => {
             context.log('Got base coupon to start from');
@@ -53,39 +55,39 @@ module.exports = function (context, data) {
                     context.done(error);
                 }
             });
-        });
-        */
+            
+        });*/
+        
     } catch (e) {
         context.log(e.message);
     }
-
-    function generateSasToken(context, container, blobName, permissions) {
-        var connString = process.env.AzureWebJobsStorage;
-        var blobService = azure.createBlobService(connString);
-
-        // Create a SAS token that expires in an hour
-        // Set start time to five minutes ago to avoid clock skew.
-        var startDate = new Date();
-        startDate.setMinutes(startDate.getMinutes() - 5);
-        var expiryDate = new Date(startDate);
-        expiryDate.setMinutes(startDate.getMinutes() + 60);
-
-        permissions = permissions || azure.BlobUtilities.SharedAccessPermissions.READ;
-
-        var sharedAccessPolicy = {
-            AccessPolicy: {
-                Permissions: permissions,
-                Start: startDate,
-                Expiry: expiryDate
-            }
-        };
-
-        var sasToken = blobService.generateSharedAccessSignature(container, blobName, sharedAccessPolicy);
-
-        return {
-            token: sasToken,
-            uri: blobService.getUrl(container, blobName, sasToken, true)
-        };
-    }
-
 };
+
+function generateSasToken(context, container, blobName, permissions) {
+    var connString = process.env.AzureWebJobsStorage;
+    var blobService = azure.createBlobService(connString);
+
+    // Create a SAS token that expires in an hour
+    // Set start time to five minutes ago to avoid clock skew.
+    var startDate = new Date();
+    startDate.setMinutes(startDate.getMinutes() - 5);
+    var expiryDate = new Date(startDate);
+    expiryDate.setMinutes(startDate.getMinutes() + 60);
+
+    permissions = permissions || azure.BlobUtilities.SharedAccessPermissions.READ;
+
+    var sharedAccessPolicy = {
+        AccessPolicy: {
+            Permissions: permissions,
+            Start: startDate,
+            Expiry: expiryDate
+        }
+    };
+
+    var sasToken = blobService.generateSharedAccessSignature(container, blobName, sharedAccessPolicy);
+
+    return {
+        token: sasToken,
+        uri: blobService.getUrl(container, blobName, sasToken, true)
+    };
+}
