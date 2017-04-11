85949aa2bc08e058659bab980d6bb5c99536246b
Ahmed Sabbour <sabb0ur@gmail.com>
2017-04-11 17:12:01 +0200

    Added exception handling

--- FunctionApp/GenerateCoupon/index.js
+++ FunctionApp/GenerateCoupon/index.js
@@ -3,53 +3,57 @@ var path = require("path");
 var azure = require('azure-storage');
 
 module.exports = function (context, data) {
-    var customerName = data.CustomerName;
+    try {
+        var customerName = data.body.CustomerName;
 
-    // Log incoming request:
-    context.log('Got a request to create coupon for customer: ' + customerName);
+        // Log incoming request:
+        context.log('Got a request to create coupon for customer: ' + customerName);
 
-    // Determine where base image is:
-    var baseImgPath = path.resolve(__dirname, 'coupon.jpg');
-    context.log('Base image path: ' + baseImgPath);
+        // Determine where base image is:
+        var baseImgPath = path.resolve(__dirname, 'coupon.jpg');
+        context.log('Base image path: ' + baseImgPath);
 
-    // Read image with Jimp
-    Jimp.read(baseImgPath).then((image) => {
-        context.log('Got base coupon to start from');
+        // Read image with Jimp
+        Jimp.read(baseImgPath).then((image) => {
+            context.log('Got base coupon to start from');
 
-        Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(function (font) {
-            context.log('Loaded font for writing on image.');
+            Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(function (font) {
+                context.log('Loaded font for writing on image.');
 
-            // Write discount on image:
-            image.print(font, 50, 50, "25% Discount");
+                // Write discount on image:
+                image.print(font, 50, 50, "25% Discount");
 
-            Jimp.loadFont(Jimp.FONT_SANS_8_BLACK).then(function (font) {
-                // Write customerName on image:
-                image.print(font, 50, 150, customerName);
+                Jimp.loadFont(Jimp.FONT_SANS_8_BLACK).then(function (font) {
+                    // Write customerName on image:
+                    image.print(font, 50, 150, customerName);
 
-                // Manipulate image
-                image.getBuffer(Jimp.MIME_JPEG, (error, stream) => {
-                    context.log(`Successfully processed the image`);
+                    // Manipulate image
+                    image.getBuffer(Jimp.MIME_JPEG, (error, stream) => {
+                        context.log(`Successfully processed the image`);
 
-                    var blobLocation = generateSasToken(context, 'coupons', AttemptId + '.jpg', 'r').uri;
-                    context.log('Coupons to be stored here: ' + blobLocation);
+                        var blobLocation = generateSasToken(context, 'coupons', AttemptId + '.jpg', 'r').uri;
+                        context.log('Coupons to be stored here: ' + blobLocation);
 
-                    // Return url to storage:
-                    context.res = {
-                        body: { couponUrl: blobLocation }
-                    };
+                        // Return url to storage:
+                        context.res = {
+                            body: { couponUrl: blobLocation }
+                        };
 
-                    // Bind the stream to the output binding to create a new blob
-                    context.done(null, { outputBlob: stream });
+                        // Bind the stream to the output binding to create a new blob
+                        context.done(null, { outputBlob: stream });
+                    });
                 });
-            });
 
-            // Check for errors
-            if (error) {
-                context.log(`There was an error processing the image.`);
-                context.done(error);
-            }
+                // Check for errors
+                if (error) {
+                    context.log(`There was an error processing the image.`);
+                    context.done(error);
+                }
+            });
         });
-    });
+    } catch (e) {
+        context.log(e.message);
+    }
 
     function generateSasToken(context, container, blobName, permissions) {
         var connString = process.env.AzureWebJobsStorage;
